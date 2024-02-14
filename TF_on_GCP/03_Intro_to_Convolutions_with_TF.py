12af894dae99ed56a5f23175be0ae739346f1d25
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2024-02-14 19:32:37 +0000

    Update 03_Intro_to_Convolutions_with_TF.py

--- TF_on_GCP/03_Intro_to_Convolutions_with_TF.py
+++ TF_on_GCP/03_Intro_to_Convolutions_with_TF.py
@@ -53,3 +53,29 @@ for x in range(1,size_x-1):
       if(convolution>255):
         convolution=255
       i_transformed[x, y] = convolution
+
+
+
+new_x = int(size_x/2)
+new_y = int(size_y/2)
+newImage = np.zeros((new_x, new_y))
+for x in range(0, size_x, 2):
+  for y in range(0, size_y, 2):
+    pixels = []
+    pixels.append(i_transformed[x, y])
+    pixels.append(i_transformed[x+1, y])
+    pixels.append(i_transformed[x, y+1])
+    pixels.append(i_transformed[x+1, y+1])
+    pixels.sort(reverse=True)
+    newImage[int(x/2),int(y/2)] = pixels[0]
+
+# Plot the image. Note the size of the axes -- now 256 pixels instead of 512
+plt.gray()
+plt.grid(False)
+plt.savefig('final.png')
+plt.imshow(newImage)
+#plt.axis('off')
+plt.show()
+
+
+
